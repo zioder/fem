0b8c81954ba84c346885087214e87a5ad7cab763
zioder <ziedkallel97@gmail.com>
2023-08-18 22:19:09 +0100

    Login/Signup page redesign + google sign in trial

--- lib/services/authservice.dart
+++ lib/services/authservice.dart
@@ -1,5 +1,27 @@
 
+import 'dart:ffi';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 class AuthService {
-  late final  User;
+ FirebaseAuth _auth = FirebaseAuth.instance;
+ final GoogleSignIn _googleSignIn= GoogleSignIn();
+
+ Future<void> handleSignIn()async {
+  try {
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    if (googleUser != null){
+      GoogleSignInAuthentication googleAuth= = await googleUser.authentication;
+     AuthCredential credential= GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      await _auth.signInWithCredential(credential);
+    }
+  }
+  catch(e){};
+
+ }
 
 }
